cc99be6d78d17a2ddbbb806b016e862a1ddc0b81
Justin Le <justin@jle.im>
2019-11-04 22:16:04 -0800

    did i do this right

--- dist/utils/testUtils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getInputName(name) {
-    return `INPUT_${name.replace(/ /g, "_").toUpperCase()}`;
-}
-function setInput(name, value) {
-    process.env[getInputName(name)] = value;
-}
-exports.setInput = setInput;
